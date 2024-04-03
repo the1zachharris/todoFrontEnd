a9f64b8522bf0a565bf384a9699f94c5a40c71f5
Zachary Harris <the1zachharris@gmail.com>
2024-04-03 17:23:21 -0500

    changed prefix

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-list',
+  selector: 'todo-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css'],
+  styleUrls: ['./list.component.css']
 })
-export class ListComponent {}
+export class ListComponent {
+
+}
